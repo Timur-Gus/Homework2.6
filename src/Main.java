e5f746bf8859dbe3f3aeef182c9ff83a5fac7612
Timur-Gus <timurka24478@gmail.com>
2024-01-11 16:56:02 +0300

    Заменил цикл на метод sort

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void taskOne() {
         //Задание 1
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        for (Integer num : nums) {
+        for (int num : nums) {
             if (num % 2 != 0) {
                 System.out.print(num + " ");
             }
@@ -21,22 +21,16 @@ public class Main {
 
     public static void taskTwo() {
         //Задание 2
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 88299, 3, 4, 422341, 5, 5,361, 137));
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         Set<Integer> nums1 = new HashSet<>();
-        for (Integer num : nums) {
-            if (num % 2 != 0) {
+        for (int num : nums) {
+            if (num % 2 == 0) {
                 nums1.add(num);
             }
         }
         List<Integer> nums3 = new ArrayList<>();
         nums3.addAll(nums1);
-        for (int i = 0; i < nums3.size() - 1; i++) {
-            if (nums3.get(i) > nums3.get(i + 1)) {
-                int n = nums3.get(i);
-                nums3.set(i, nums3.get(i + 1));
-                nums3.set(i + 1, n);
-            }
-        }
+        Collections.sort(nums3);
         System.out.println(nums3);
     }
 
@@ -47,15 +41,15 @@ public class Main {
         strings1.addAll(strings);
         System.out.println(strings1);
     }
+
     public static void taskFour() {
         //Задание 4
         List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
-        Map<String,Integer> strings1 = new HashMap<>();
+        Map<String, Integer> strings1 = new HashMap<>();
         for (String string : strings) {
             if (!strings1.containsKey(string)) {
                 strings1.put(string, 1);
-            }
-            else {
+            } else {
                 strings1.put(string, strings1.get(string) + 1);
             }
         }
